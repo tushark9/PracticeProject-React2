140fc681eefcf9011b032bb9dbceab322f705a2d
Tushar <tusharkathuria41051@gmail.com>
2024-09-17 19:32:24 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/project-2/",
+  base:"/PracticeProject-React2/",
   plugins: [react()],
 })
